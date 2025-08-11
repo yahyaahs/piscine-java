04f8db919bf30c7f559822f123620d7897b5b73f
yahya houssaini <erensas007@gmail.com>
2025-08-11 23:22:36 +0100

    concat

--- ExerciseRunner.java
+++ ExerciseRunner.java
@@ -1,6 +1,6 @@
 public class ExerciseRunner {
     public static void main(String[] args) {
-        System.out.println(StringLength.getStringLength("Hello World !"));
-        System.out.println(StringLength.getStringLength(null));
+        System.out.println(StringConcat.concat("Hello ", "étudiant !"));
+        System.out.println(StringConcat.concat(null, "Hello World !"));
     }
 }
---
+++ StringConcat/StringConcat.java
@@ -0,0 +1,6 @@
+public class StringConcat {
+    public static String concat(String s1, String s2) {
+        // your code here
+		return s1 +s2;
+    }
+}
