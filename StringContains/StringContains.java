6c9e07de57e4ee5f65a37f7971a63e428c1666a6
yahya houssaini <erensas007@gmail.com>
2025-08-12 00:52:47 +0100

    we check both the string and substring if they are null

--- StringContains/StringContains.java
+++ StringContains/StringContains.java
@@ -1,7 +1,7 @@
 public class StringContains {
     public static boolean isStringContainedIn(String subString, String s) {
         // your code here
-		if (s == null){
+		if (s == null || subString == null){
 			return false;
 		}
 		return s.contains(subString);
