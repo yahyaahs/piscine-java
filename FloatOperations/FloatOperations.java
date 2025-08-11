ef49ecd140db90d08464cd709e6bbd22791bf76f
yahya houssaini <erensas007@gmail.com>
2025-08-11 20:53:03 +0100

    floats

---
+++ FloatOperations/FloatOperations.java
@@ -0,0 +1,10 @@
+public class FloatOperations {
+    public static float addTwoFloats(float a, float b) {
+        // your code here
+		return a + b;
+    }
+    public static float divideTwoFloats(float a, float b) {
+        // your code here
+		return a /b;
+    }
+}
