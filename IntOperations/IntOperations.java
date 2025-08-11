71a310b390ef3831e5bc65da4d397bfe59c8ff3e
yahya houssaini <erensas007@gmail.com>
2025-08-11 20:44:41 +0100

    ops

---
+++ IntOperations/IntOperations.java
@@ -0,0 +1,18 @@
+public class IntOperations {
+    public static int addTwoIntegers(int a, int b) {
+		return a + b;
+        // your code here
+    }
+    public static int subtractTwoIntegers(int a, int b) {
+        // your code here
+		return a - b;
+    }
+    public static int multiplyTwoIntegers(int a, int b) {
+        // your code here
+		return a * b;
+    }
+    public static int divideTwoIntegers(int a, int b) {
+        // your code here
+		return a / b;
+    }
+}
