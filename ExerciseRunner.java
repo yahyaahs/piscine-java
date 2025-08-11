019038a5abff078dbd827a400f72ff36959da4d8
yahya houssaini <erensas007@gmail.com>
2025-08-12 00:50:16 +0100

    contains

--- ExerciseRunner.java
+++ ExerciseRunner.java
@@ -1,6 +1,7 @@
 public class ExerciseRunner {
     public static void main(String[] args) {
-        System.out.println(StringConcat.concat("Hello ", "étudiant !"));
-        System.out.println(StringConcat.concat(null, null));
+        System.out.println(StringContains.isStringContainedIn("Hell", "Highway to Hell"));
+        System.out.println(StringContains.isStringContainedIn("Hell", "Hello World !"));
+        System.out.println(StringContains.isStringContainedIn("Bonjour", "hello World !"));
     }
 }
---
+++ StringContains/StringContains.java
@@ -0,0 +1,6 @@
+public class StringContains {
+    public static boolean isStringContainedIn(String subString, String s) {
+        // your code here
+		return s.contains(subString);
+    }
+}
