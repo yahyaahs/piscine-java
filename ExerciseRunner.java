e302c3b12677a1a4846bce31eebebedf50c7a4a0
yahya houssaini <erensas007@gmail.com>
2025-08-12 00:59:39 +0100

    diffrent parrams

--- ExerciseRunner.java
+++ ExerciseRunner.java
@@ -1,7 +1,7 @@
 public class ExerciseRunner {
     public static void main(String[] args) {
-        System.out.println(StringContains.isStringContainedIn("Hell", "Highway to Hell"));
-        System.out.println(StringContains.isStringContainedIn("Hell", "Hello World !"));
-        System.out.println(StringContains.isStringContainedIn("Bonjour", "hello World !"));
+        System.out.println(StringReplace.replace("javatpoint is a very good website", 'a', 'e'));
+        System.out.println(StringReplace.replace("my name is khan my name is java", "is","was"));
+        System.out.println(StringReplace.replace("hey i'am java", "I'am","was"));
     }
 }
---
+++ StringReplace/StringReplace.java
@@ -0,0 +1,9 @@
+public class StringReplace {
+    public static String replace(String s, String target, String replacement) {
+        // your code here
+		return s.replace(target, replacement);
+    }
+	public static String replace(String s, char t, char rep){
+				return s.replace(t, rep);
+	}
+}
