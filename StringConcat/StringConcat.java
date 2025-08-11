5d16c87aa4dfe25d0cdecaa08064ac70841b4789
yahya houssaini <erensas007@gmail.com>
2025-08-12 00:46:18 +0100

    string concat null

--- StringConcat/StringConcat.java
+++ StringConcat/StringConcat.java
@@ -1,6 +1,12 @@
 public class StringConcat {
     public static String concat(String s1, String s2) {
         // your code here
-		return s1 +s2;
+		if(s1 == null){
+			s1= "";
+		}
+		if (s2 == null) {
+			s2="";
+		}
+		return s1+s2;
     }
 }
