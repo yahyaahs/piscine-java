2e4e284b8713665ca17dd95f2dc4056aaa0a2a7a
yahya houssaini <erensas007@gmail.com>
2025-08-12 00:52:01 +0100

    null

--- StringContains/StringContains.java
+++ StringContains/StringContains.java
@@ -1,6 +1,9 @@
 public class StringContains {
     public static boolean isStringContainedIn(String subString, String s) {
         // your code here
+		if (s == null){
+			return false;
+		}
 		return s.contains(subString);
     }
 }
