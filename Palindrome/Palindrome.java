69a16f3f93257d84480b5af4cde23dde1226674b
yahya houssaini <erensas007@gmail.com>
2025-08-12 05:44:06 +0100

    case sesitivity

--- Palindrome/Palindrome.java
+++ Palindrome/Palindrome.java
@@ -1,11 +1,15 @@
 public class Palindrome {
     public static boolean isPalindrome(String s) {
+		if (s.isEmpty() || s == null){
+			return true;
+		}
         // your code here
 		String n = "";
 		for (int i = s.length()-1; i>=0; i--){
 			n+= s.charAt(i);
 		}
 
-		return s.equals(n);
+
+		return s.toLowerCase().equals(n.toLowerCase());
 	}
 }
