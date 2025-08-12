da4a474bd4c47513b62ab6e07664d314ff8a5330
yahya houssaini <erensas007@gmail.com>
2025-08-12 06:08:46 +0100

    null array

--- ComputeArray/ComputeArray.java
+++ ComputeArray/ComputeArray.java
@@ -1,6 +1,8 @@
 public class ComputeArray {
     public static int[] computeArray(int[] array) {
-
+		if (array == null){
+			return null;
+		}
 		for(int i  = 0; i<array.length; i++){
 			if(array[i]%3 == 0){
 				array[i] = array[i]*5;
