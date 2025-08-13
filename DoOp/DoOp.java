c04ef5df9c5df81b454d56c2fc60ea04dcffe841
yahya houssaini <erensas007@gmail.com>
2025-08-13 19:27:05 +0100

    null

--- DoOp/DoOp.java
+++ DoOp/DoOp.java
@@ -1,18 +1,36 @@
 public class DoOp {
-    public static String operate(String[] args) {
-		if (args.length < 3){
+	public static String operate(String[] args) {
+		if (args.length < 3) {
 			return null;
 		}
-		if (args[1]== "+"){
+		if (args[0] ==null || args[1]== null || args[2]==null){
+			return null;
+		}
+		if (args[1] == "+") {
+
 			int i = Integer.parseInt(args[0]) + Integer.parseInt(args[2]);
 			return String.valueOf(i);
 		}
-		if (args[1]== "-"){
-				int i = Integer.parseInt(args[0]) - Integer.parseInt(args[2]);
+		if (args[1] == "-") {
+			int i = Integer.parseInt(args[0]) - Integer.parseInt(args[2]);
 			return String.valueOf(i);
 		}
-		if (args[1]== "%"){
-				int i = Integer.parseInt(args[0]) % Integer.parseInt(args[2]);
+		if (args[1] == "%") {
+			if (args[2] == "0") {
+				return "0";
+			}
+			int i = Integer.parseInt(args[0]) % Integer.parseInt(args[2]);
+			return String.valueOf(i);
+		}
+		if (args[1] == "/") {
+			if (args[2] == "0") {
+				return "0";
+			}
+			int i = Integer.parseInt(args[0]) / Integer.parseInt(args[2]);
+			return String.valueOf(i);
+		}
+		if (args[1] == "*") {
+			int i = Integer.parseInt(args[0]) * Integer.parseInt(args[2]);
 			return String.valueOf(i);
 		}
 		return null;
