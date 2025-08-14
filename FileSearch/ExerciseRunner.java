0ab7915a3bcd0e4e58d8a322e94c0975455de5f2
yahya houssaini <erensas007@gmail.com>
2025-08-14 07:34:37 +0100

    static final

--- FileSearch/ExerciseRunner.java
+++
@@ -1,7 +0,0 @@
-import java.io.IOException;
-
-public class ExerciseRunner {
-    public static void main(String[] args) throws IOException {
-        System.out.println(FileSearch.searchFile("file.txt"));
-    }
-}
