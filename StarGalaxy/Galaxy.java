e5a424a4c729adaf5de0589b35e49fca5f75e24b
yahya houssaini <erensas007@gmail.com>
2025-08-15 14:29:57 +0100

    fix spelling

--- StarGalaxy/Galaxy.java
+++ StarGalaxy/Galaxy.java
@@ -1,15 +1,14 @@
-package StarGalaxy;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Galaxy {
 
     private List<CelestialObject> celestialObjects;
-    
+
     public Galaxy(){
         celestialObjects = new ArrayList<CelestialObject>();
     }
-    public List<CelestialObject> getCelestianObjects(){
+    public List<CelestialObject> getCelestialObjects(){
         return celestialObjects;
     }
     public void addCelestialObject(CelestialObject obj){
