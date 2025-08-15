17b8a76ce1778012c651c01ca2d35661da5c3887
yahya houssaini <erensas007@gmail.com>
2025-08-15 13:11:43 +0100

    inherit

---
+++ StarInheritance/Star.java
@@ -0,0 +1,9 @@
+public class Star extends CelestialObject{
+    private double magnitude;
+    public void setMagnitude(double m){
+        magnitude = m;
+    }
+    public double getMagnitude(){
+        return magnitude;
+    }
+}
