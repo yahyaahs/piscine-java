f22880f3c548ab1288c20be44a8ace687f58b61b
yahya houssaini <erensas007@gmail.com>
2025-08-15 13:38:45 +0100

    star overide

--- ExerciseRunner.java
+++ ExerciseRunner.java
@@ -2,11 +2,12 @@ public class ExerciseRunner {
 
     public static void main(String[] args) {
         Star star = new Star();
+        Star star2 = new Star();
+        Star proxima = new Star("Proxima", 18.389, 832.32, 218, 0.4);
         
-        System.out.println(star.getName());
-        System.out.println(star.getX());
-        System.out.println(star.getY());
-        System.out.println(star.getZ());
-        System.out.println(star.getMagnitude());
+        System.out.println(star.toString());
+        System.out.println(proxima.toString());
+        System.out.println(star.equals(star2));
+        System.out.println(star.equals(proxima));
     }
 }
--- StarOverride/Star.java
+++ StarOverride/Star.java
@@ -2,19 +2,24 @@ import java.util.Objects;
 
 public class Star extends CelestialObject{
     private double magnitude;
+    public Star(String name, double x, double y, double z, double magnitude){
+        super(name, x, y, z);
+        this.magnitude = magnitude;
+    }
+    public Star(){
+        super();
+        magnitude = 0.0;
+    }
     public void setMagnitude(double m){
         magnitude = m;
     }
     public double getMagnitude(){
         return magnitude;
     }
-    public Star(String name, double x, double y, double z, double magnitude){
-        super(name, x, y, z);
-        this.magnitude = magnitude;
-    }
+  
     @Override
     public String toString(){
-        return String.format("%s shines at the  %.3f", name, magnitude);
+        return String.format("%s shines at the %.3f magnitude", name, magnitude);
     }
     @Override
     public boolean equals(Object obj){
