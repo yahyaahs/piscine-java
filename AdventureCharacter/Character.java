90feada574cf2dd6992e7dd87ca7d85d61f45f91
yahya houssaini <erensas007@gmail.com>
2025-08-18 11:42:28 +0100

    charachter

---
+++ AdventureCharacter/Character.java
@@ -0,0 +1,37 @@
+public class Character {
+    public final int maxHealth;
+    public int currentHealth;
+    public final String name;
+
+
+    public Character(String name, int maxHealth){
+        this.name = name;
+        this.maxHealth = maxHealth;
+        this.currentHealth = maxHealth;
+    }
+    public int getmaxHealth(){
+        return maxHealth;
+    }
+    public int getcurrentHealth(){
+        return currentHealth;
+    }
+    public String getname(){
+        return name;
+    }
+    public void takeDamage(int sub){
+        if(currentHealth - sub < 0){
+            return;
+        }else{
+            currentHealth = currentHealth - sub;
+        }
+    }
+    public void attack(Character c){
+        c.takeDamage(9);
+    }
+    public String toString(){
+        if (currentHealth == 0){
+            return String.format("%s : KO", name);
+        }
+        return String.format("%s : %d/%d", name, currentHealth, maxHealth);
+    }
+}
