cb61408920de1d524840ae0248f34271cb89ce1b
yahya houssaini <erensas007@gmail.com>
2025-08-18 11:50:18 +0100

    private name field

--- AdventureCharacter/Character.java
+++ AdventureCharacter/Character.java
@@ -1,7 +1,7 @@
 public class Character {
-    public final int maxHealth;
+    private final int maxHealth;
     public int currentHealth;
-    public final String name;
+    private final String name;
 
 
     public Character(String name, int maxHealth){
