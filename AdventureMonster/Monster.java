703d2af9b5fe9e0f11c144933fa569272fbed1bc
yahya houssaini <erensas007@gmail.com>
2025-08-19 16:15:28 +0100

    monster

---
+++ AdventureMonster/Monster.java
@@ -0,0 +1,19 @@
+package AdventureMonster;
+
+public class Monster extends Character {
+    private final int maxHealth;
+    private final String name;
+    public Monster(String name, int maxHealth){
+        super(name, maxHealth);
+        this.name = name;
+        this.maxHealth = maxHealth;
+    }
+    @Override
+    public String toString(){
+        if(getCurrentHealth() != 0){
+            return String.format("%s is a monster with %d HP", this.name, getCurrentHealth());
+        }else{
+            return String.format("%s is a monster and is dead", name);
+        }
+    }
+}
