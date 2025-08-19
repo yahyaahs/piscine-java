a4ca96ce30f0b523d4441ed1b0d683afc17540a3
yahya houssaini <erensas007@gmail.com>
2025-08-19 18:16:56 +0100

    check

--- AdventureSorcerer/Sorcerer.java
+++ AdventureSorcerer/Sorcerer.java
@@ -22,7 +22,7 @@ public class Sorcerer extends Character implements Healer  {
     }
     @Override
     public String toString(){
-        if(this.getCurrentHealth() == 0){
+        if(getCurrentHealth() != 0){
             return String.format("%s is a sorcerer with %d HP. It can heal %d HP.", this.name, this.getCurrentHealth(), healCapacity);
         }else{
             return String.format("%s is a dead sorcerer. So bad, it could heal %d HP.", this.name, this.healCapacity);
