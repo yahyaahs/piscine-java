89621bea1d63272c4e8b46107a12da5c92d9f2ff
yahya houssaini <erensas007@gmail.com>
2025-08-19 18:44:35 +0100

    templat

---
+++ AdventureTemplar/Templar.java
@@ -0,0 +1,33 @@
+public class Templar extends Character implements Healer, Tank{
+    private final int healCapacity;
+    private final int sheild;
+    public Templar(String name, int maxHealth, int healCapacity, int sheild){
+        super(name, maxHealth);
+        this.healCapacity = healCapacity;
+        this.sheild = sheild ;
+    }
+    @Override
+    public int getHealCapacity(){
+        return healCapacity;
+    }
+    @Override
+    public void heal(Character c){
+        if(c.getCurrentHealth()+ healCapacity > c.getMaxHealth()){
+            c.setCurrentHealth(c.getMaxHealth());
+            return;
+        }
+        c.setCurrentHealth(c.getCurrentHealth()+ healCapacity);
+    }
+    @Override
+    public int getShield(){
+        return sheild;
+    }
+    @Override
+    public String toString(){
+        if(getCurrentHealth() == 0){
+            return String.format("%s has been beaten, even with its %d shield. So bad, it could heal %d HP.", getName(), getShield(),getHealCapacity());
+        }else{
+            return String.format("%s is a strong Templar with %d HP. It can heal %d HP and has a shield of %d.", getName(), getCurrentHealth(), getHealCapacity(), getShield());
+        }
+    }
+}
