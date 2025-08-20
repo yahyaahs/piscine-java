ae5c9f05f94505274866cfd46f9debf327831798
yahya houssaini <erensas007@gmail.com>
2025-08-20 12:13:14 +0100

    protect setcurrent health

--- AdventureAbstract/Character.java
+++ AdventureAbstract/Character.java
@@ -14,7 +14,7 @@ abstract public class Character {
         this.currentHealth = maxHealth;
         allCharacters.add(this);
     }
-    public void setCurrentHealth(int n){
+    private void setCurrentHealth(int n){
         this.currentHealth = n;
     }
  
