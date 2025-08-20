92c327c8460f6770eb4791d6055acad9d9a5f62b
yahya houssaini <erensas007@gmail.com>
2025-08-20 19:02:45 +0100

    null

--- ListSearchIndex/ListSearchIndex.java
+++ ListSearchIndex/ListSearchIndex.java
@@ -29,7 +29,7 @@ public class ListSearchIndex {
     }
     public static List<Integer> findAllIndexes(List<Integer> list, Integer value) {
         // your code here
-        if (list == null || value == null){
+        if (list.equals(null)){
             return null;
         }
           if(list.isEmpty()){
