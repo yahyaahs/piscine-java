415384f1c0d76bf78af90d48ede46157601ab938
yahya houssaini <erensas007@gmail.com>
2025-08-20 18:52:56 +0100

    indexes

--- ListSearchIndex/ListSearchIndex.java
+++ ListSearchIndex/ListSearchIndex.java
@@ -4,14 +4,32 @@ import java.util.ArrayList;
 public class ListSearchIndex {
     public static Integer findLastIndex(List<Integer> list, Integer value) {
         // your code her
-        return list.lastIndexOf(value);
-    }
+        if(list.isEmpty()){
+            return null;
+        }
+   int index = list.lastIndexOf(value);
+        if(index == -1){
+            return null;
+        }else{
+            return index;
+        }    }
     public static Integer findFirstIndex(List<Integer> list, Integer value) {
         // your code here
-        return list.indexOf(value);
+          if(list.isEmpty()){
+            return null;
+        }
+        int index = list.indexOf(value);
+        if(index == -1){
+            return null;
+        }else{
+            return index;
+        }
     }
     public static List<Integer> findAllIndexes(List<Integer> list, Integer value) {
         // your code here
+          if(list.isEmpty()){
+            return null;
+        }
         List<Integer> matchingIndices = new ArrayList<>();
         for (int i = 0; i < list.size(); i++) {
             if (value.equals(list.get(i))) {
