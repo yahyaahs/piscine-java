ba8b4ebe875a70d28bd70c487f6999d7a6e89a10
yahya houssaini <erensas007@gmail.com>
2025-08-20 17:38:05 +0100

    equals

--- ListEquals/ListEquals.java
+++ ListEquals/ListEquals.java
@@ -3,8 +3,8 @@ import java.util.List;
 public class ListEquals {
     public static boolean areListsEqual(List<String> list1, List<String> list2) {
         // your code here
-        if(list1 == null || list2 == null){
-            return false;
+        if(list1 == null && list2 == null){
+            return true;
         }
         return list1.equals(list2);
     }
