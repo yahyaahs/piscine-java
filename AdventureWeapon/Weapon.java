29d2f44556055db9a384542cbef48843ea3adcfa
yahya houssaini <erensas007@gmail.com>
2025-08-20 14:58:21 +0100

    rivate

--- AdventureWeapon/Weapon.java
+++ AdventureWeapon/Weapon.java
@@ -1,6 +1,6 @@
 public class Weapon {
     private final String name;
-    private final int damage;
+    protected final int damage;
     public Weapon(String name, int damage){
         this.name = name;
         this.damage = damage;
