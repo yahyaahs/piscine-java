012990c3a849c5a65287e8e5d0014eb2c3889bae
yahya houssaini <erensas007@gmail.com>
2025-08-20 17:36:17 +0100

    equals

---
+++ ListEquals/ListEquals.java
@@ -0,0 +1,11 @@
+import java.util.List;
+
+public class ListEquals {
+    public static boolean areListsEqual(List<String> list1, List<String> list2) {
+        // your code here
+        if(list1 == null || list2 == null){
+            return false;
+        }
+        list1.equals(list2);
+    }
+}
