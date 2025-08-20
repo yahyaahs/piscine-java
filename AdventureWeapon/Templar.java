cf8e8e7760f5a7bf9cbfedfd94a900689e0025f0
yahya houssaini <erensas007@gmail.com>
2025-08-20 13:10:23 +0100

    weapon

---
+++ AdventureWeapon/Templar.java
@@ -0,0 +1,45 @@
+public class Templar extends Character implements Healer, Tank{
+    private final int healCapacity;
+    private final int shield;
+    public Templar(String name, int maxHealth, int healCapacity, int shield){
+        super(name, maxHealth);
+        this.healCapacity = healCapacity;
+        this.shield = shield ;
+    }
+    @Override
+    public int getHealCapacity(){
+        return healCapacity;
+    }
+    @Override
+    public void heal(Character c){
+        if(c.getCurrentHealth()+ healCapacity > c.getMaxHealth()){
+            c.setCurrentHealth(c.getMaxHealth());
+            return;
+        }
+        c.setCurrentHealth(c.getCurrentHealth()+ healCapacity);
+    }
+    @Override
+    public int getShield(){
+        return shield;
+    }
+    @Override
+    public String toString(){
+        if(getCurrentHealth() == 0){
+            return String.format("%s has been beaten, even with its %d shield. So bad, it could heal %d HP. %s", getName(), getShield(),getHealCapacity(), weapon.toString());
+        }else{
+            return String.format("%s is a strong Templar with %d HP. It can heal %d HP and has a shield of %d. %s", getName(), getCurrentHealth(), getHealCapacity(), getShield(), weapon.toStirng());
+        }
+    }
+
+    public void attack(Character c) {
+        heal(this)  ;
+        if(damage == null){
+            c.takeDamage(6);
+        }else {
+            c.takeDamage(damage);
+        }
+    }
+    public void takeDamage(int sub) {
+        setCurrentHealth(Math.max(getCurrentHealth() - (sub- shield), 0));
+    }
+}
---
+++ AdventureWeapon/Weapon.java
@@ -0,0 +1,19 @@
+public class Weapon {
+    private final String name;
+    protected final int damge;
+    public Weapon(String name, int damage){
+        this.name = name;
+        this.damge = damage;
+    }
+
+    public int getDamge() {
+        return damge;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public String toString(){
+        return  String.format("%s deals %d damages", name, damge);
+    }
+}
