950635063cd8cdb97356c90abe1b1dfd0bc8be5a
yahya houssaini <erensas007@gmail.com>
2025-08-20 14:56:01 +0100

    qlskdf

--- AdventureWeapon/Weapon.java
+++ AdventureWeapon/Weapon.java
@@ -1,19 +1,19 @@
 public class Weapon {
     private final String name;
-    protected final int damge;
+    protected final int damage;
     public Weapon(String name, int damage){
         this.name = name;
         this.damge = damage;
     }
 
     public int getDamage() {
-        return damge;
+        return damage;
     }
 
     public String getName() {
         return name;
     }
     public String toString(){
-        return  String.format("%s deals %d damages", name, damge);
+        return  String.format("%s deals %d damages", name, damage);
     }
 }
