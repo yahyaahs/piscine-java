aed2a1bcf69361bb64951cb80a0dad22a0da5ece
yahya houssaini <erensas007@gmail.com>
2025-08-23 16:51:21 +0100

    wedding

---
+++ Wedding/Test.java
@@ -0,0 +1,8 @@
+import java.util.Set;
+
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println(Wedding.createCouple(Set.of("Pikachu", "Dracaufeu", "Tortank"), Set.of("Legolas", "Aragorn", "Gimli")));
+    }
+}
---
+++ Wedding/Wedding.java
@@ -0,0 +1,20 @@
+import java.util.Set;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+public class Wedding {
+    public static Map<String, String> createCouple(Set<String> first, Set<String> second) {
+        // your code here
+        Map<String, String> m = new HashMap<>();
+        List<String> key = new ArrayList<>(first);
+        List<String> val = new ArrayList<>(first);
+
+        for(int i = 0; i<Math.max(key.size(), val.size()); i++){
+            m.put(key.get(i), val.get(i));
+        }
+        return m;
+    }
+}
