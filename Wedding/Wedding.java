eefe30703d91e2a0e1e824b6df6d85c91fff0078
yahya houssaini <erensas007@gmail.com>
2025-08-23 16:53:29 +0100

    min

--- Wedding/Wedding.java
+++ Wedding/Wedding.java
@@ -10,7 +10,7 @@ public class Wedding {
         // your code here
         Map<String, String> m = new HashMap<>();
         List<String> key = new ArrayList<>(first);
-        List<String> val = new ArrayList<>(first);
+        List<String> val = new ArrayList<>(second);
 
         for(int i = 0; i<Math.min(key.size(), val.size()); i++){
             m.put(key.get(i), val.get(i));
