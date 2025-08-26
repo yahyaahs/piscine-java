9128c667f252c8161339c3662abc2ddfc0205547
yahya houssaini <erensas007@gmail.com>
2025-08-26 15:32:03 +0100

    check negative

--- DifferenceBetweenDate/DifferenceBetweenDate.java
+++ DifferenceBetweenDate/DifferenceBetweenDate.java
@@ -8,17 +8,23 @@ public class DifferenceBetweenDate {
 
     public static Duration durationBetweenTime(LocalTime localTime1, LocalTime localTime2) {
         // your code here
-        
+
         return Duration.between(localTime1, localTime2).abs();
     }
 
     public static Period periodBetweenDate(LocalDate date1, LocalDate date2) {
         // your code here
+        if(date1.isAfter(date2)){
+            return Period.between(date2, date1);
+        }
         return Period.between(date1, date2);
     }
 
     public static Long numberOfHoursBetweenDateTime(LocalDateTime dateTime1, LocalDateTime dateTime2) {
         // your code here
+        if(dateTime1.isAfter(dateTime2)){
+            return Duration.between(dateTime2, dateTime1).toHours();
+        }
         return Duration.between(dateTime1, dateTime2).toHours();
     }
         public static void main(String[] args) {
