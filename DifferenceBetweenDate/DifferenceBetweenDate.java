d5c632f0d41304b8f6f32369b187f5234c81ec94
yahya houssaini <erensas007@gmail.com>
2025-08-26 15:33:16 +0100

    null

--- DifferenceBetweenDate/DifferenceBetweenDate.java
+++ DifferenceBetweenDate/DifferenceBetweenDate.java
@@ -8,12 +8,17 @@ public class DifferenceBetweenDate {
 
     public static Duration durationBetweenTime(LocalTime localTime1, LocalTime localTime2) {
         // your code here
-
+        if (localTime1 == null || localTime2 == null) {
+            return null;
+        }
         return Duration.between(localTime1, localTime2).abs();
     }
 
     public static Period periodBetweenDate(LocalDate date1, LocalDate date2) {
         // your code here
+        if(date1 == null || date2 == null){
+            return null;
+        }
         if(date1.isAfter(date2)){
             return Period.between(date2, date1);
         }
@@ -22,6 +27,9 @@ public class DifferenceBetweenDate {
 
     public static Long numberOfHoursBetweenDateTime(LocalDateTime dateTime1, LocalDateTime dateTime2) {
         // your code here
+        if (dateTime1 == null || dateTime2 == null) {
+            return null;
+        }
         if(dateTime1.isAfter(dateTime2)){
             return Duration.between(dateTime2, dateTime1).toHours();
         }
